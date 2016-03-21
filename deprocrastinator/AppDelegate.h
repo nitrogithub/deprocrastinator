e455a8aca4721ad0792b856e1fcc8f91ab493550
kk <github@mantaone.com>
2016-03-21 13:46:37 -0700

    Initial Commit

---
+++ deprocrastinator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  deprocrastinator
+//
+//  Created by Kevin Kim on 3/21/16.
+//  Copyright © 2016 Kevin Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ deprocrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  deprocrastinator
+//
+//  Created by Kevin Kim on 3/21/16.
+//  Copyright © 2016 Kevin Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
